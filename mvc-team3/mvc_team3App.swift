9a602f8df05e4cce3006e30b64508cda9bc05769
Jemi Yantika Damanik <jemidamanik2300@gmail.com>
2022-05-11 17:46:11 +0700

    Initial Commit

---
+++ mvc-team3/mvc_team3App.swift
@@ -0,0 +1,17 @@
+//
+//  mvc_team3App.swift
+//  mvc-team3
+//
+//  Created by Jemi Damanik on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct mvc_team3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
