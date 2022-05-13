bdb3aeebd3d169a375d6ef2c2a8b536c06b80281
Aulia Rahmi <aura24011998@gmail.com>
2022-05-13 21:48:32 +0700

    feat/add-grid-main-screen
    tampilan sementara main screen

--- mvc-team3/Views/MainScreenView.swift
+++ mvc-team3/Views/MainScreenView.swift
@@ -10,17 +10,42 @@ import SwiftUI
 struct MainScreenView: View {
     @State private var showingSheet = false
     @State private var searchText = ""
+    //contoh dulu
+    var data: Int = 3
+    let items = Array(1...5).map({"\($0) Avatar : The Legend of Aang"})
+    
+    let layout = [
+        GridItem(.adaptive(minimum:150))
+    ]
+    //------
+    
     var body: some View {
-        
-        
         HStack{
             VStack{
-                Text("Tap + to add new book folder")
-                
-//                    .searchable(text: $searchText, prompt: "Looking for book folder?")
-                
+                if(data==0){
+                    Text("Tap + to add new book folder")
+                }
+                else{
+                    ScrollView(.vertical){
+                        LazyVGrid(columns: layout, content: {
+                            ForEach(items, id: \.self){ item in
+                                NavigationLink(destination: InsideBookView()){
+                                    VStack {
+                                        Image("bookSmall")
+                                            .resizable()
+                                            .frame(width: 110, height: 140)
+                                        Text(item).bold().foregroundColor(.black)
+                                            .font(.caption)
+                                            .padding(.horizontal)
+                                        Text("12 items").foregroundColor(.secondary).font(.caption2)
+                                            .padding(.bottom)
+                                    }
+                                }
+                            }
+                        })
+                    }
+                }
             }.searchable(text: $searchText, prompt: "Looking for book folder?")
-            
         }
         .background(
             Image("background")
@@ -42,10 +67,14 @@ struct MainScreenView: View {
                 AddBookView()
             }
         )
-        
-        
+                
     }
-    
+}
+
+struct InsideBookView: View{
+    var body: some View {
+        Text("OK")
+    }
 }
 
 
